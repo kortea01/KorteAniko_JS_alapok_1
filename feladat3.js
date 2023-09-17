cf300101deffe46dfd29f9ed60dafcedfbeba36e
Koerte Aniko <korte.aniko@gmail.com>
2023-09-17 21:57:03 +0200

    3. feladat megoldás

--- feladat3.js
+++ feladat3.js
@@ -1,11 +1,2 @@
-const array =[0,7,16,30,41]
-const reversedArray = []
-
-for(let i = array.length - 1; i >= 0; i--) 
-{
-  const valueAtIndex = array[i]
-  
-  reversedArray.push(valueAtIndex)
-}
-
-console.log(reversedArray)
+const lista =[0,7,16,30,41]
+for (let i = lista.length; i >= 0; i--) {console.log(lista[i]);}
